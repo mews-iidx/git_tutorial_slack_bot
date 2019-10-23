1c73f145496a7c974d03178b720cb09fe19bddba
y.yamauchi <y.yamauchi@edge-consulting.jp>
2019-10-23 11:55:37 +0900

    first version

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-108979309618-793958307315-0tvwKp8XA4GsJpYAxYr2JcrM"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
